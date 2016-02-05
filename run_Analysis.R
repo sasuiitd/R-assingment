440d86eb1c464f7066a1651b384af516562de9bd
Saurabh Suman <sasuiitd@users.noreply.github.com>
2016-02-05 20:17:48 +0530

    Update run_Analysis.R

--- run_Analysis.R
+++ run_Analysis.R
@@ -1,13 +1,11 @@
 #Coursera getting and cleaning data project
-#Data set used- https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
-#Action Required -
+#5 step process to get to the tiday data set
    #1.Merge training and test data
    #2.Only columns with mean and std extracted
-   #3.name the acivities in the data set
+   #3.Name the acivities in the data set
    #4.label the data set with activity names
    #5.Create Tidy data set
 
-#Now, let me list down code for each of the above steps
 
 #1.Merge the training and test data
 
